d22f40aa00796307d1ab98a6945796414ad973e3
kennethDRF <kennethramirezfernandez@gmail.com>
2025-02-04 10:14:11 -0600

    agregando variables

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+const inputAmigo = document.getElementById("amigo");
+const resultadoAmigoSecreto = document.getElementById("resultado");
+const listaAmigos = document.getElementById("listaAmigos");
