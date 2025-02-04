1bbf3249807e414e1f445e6c291a7fd443695355
kennethDRF <kennethramirezfernandez@gmail.com>
2025-02-04 10:15:58 -0600

    Agregando funcionalidad Lista Amigos

--- app.js
+++ app.js
@@ -5,3 +5,27 @@ let amigos = [];
 const inputAmigo = document.getElementById("amigo");
 const resultadoAmigoSecreto = document.getElementById("resultado");
 const listaAmigos = document.getElementById("listaAmigos");
+
+function limpiarInput (input) {
+  input.value=''
+}
+
+function actualizarListaAmigos() {
+  listaAmigos.innerHTML = "";
+  for (let amigo = 0; amigo < amigos.length; amigo++) {
+    listaAmigos.innerHTML += `<li>${amigos[amigo]}</li>`;
+  }
+}
+
+function agregarAmigo() {
+  let amigo = inputAmigo.value;
+  
+  if (amigo == "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+
+  amigos.push(amigo);
+  limpiarInput (inputAmigo);
+  actualizarListaAmigos();
+}
