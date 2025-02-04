c7698b0c0ad0817c1c7c445262bd80fd383e4cc3
kennethDRF <kennethramirezfernandez@gmail.com>
2025-02-04 10:16:40 -0600

    Agregando funcionalidad sorteo

--- app.js
+++ app.js
@@ -29,3 +29,15 @@ function agregarAmigo() {
   limpiarInput (inputAmigo);
   actualizarListaAmigos();
 }
+
+function sortearAmigo() {
+  if (amigos.length = 0) {
+    alert("No hay amigos que sortear")
+    return
+  }
+
+  let numeroAElegir = Math.floor(Math.random()*(amigos.length-1))+1
+  resultadoAmigoSecreto.innerHTML = `<li>El amigo secreto sorteado es: ${amigos[numeroAElegir]}</li>`;
+  amigos.pop(numeroAElegir);
+  actualizarListaAmigos();
+}
